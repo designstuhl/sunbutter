c045ada19f68fba8db331a0563b6d0df8bc45a4e
ryan <ryan@suntouchersoftware.com>
2016-01-26 15:52:56 -0800

    Put some working code into the sunbutter.rb file. Tested in console.

--- lib/sunbutter.rb
+++ lib/sunbutter.rb
@@ -1,25 +1,21 @@
-module Sunbutter
-  def self.included(base)
-    base.extend ClassMethods
+class Sunbutter
+  def self.sunbutter(logged_item = "")
+    puts "   ."
+    puts "  ..."
+    puts " ....."
+    puts "......."
+    puts " ....."
+    puts "  ..."
+    puts "   ."
+    puts ""
+    puts logged_item
+    puts ""
+    puts "   ."
+    puts "  ..."
+    puts " ....."
+    puts "......."
+    puts " ....."
+    puts "  ..."
+    puts "   ."
   end
-
-  module ClassMethods
-    def sunbutter(*args, &block)
-      # Do some stuff here
-      # I want to give users the ability to call sunbutter
-      # when using debugging 'puts' calls in their code
-      # and display their 'puts' call in the server log
-      # wrapped in some space with an obvious visual reference
-      # for example:
-      # puts ""
-      # puts "**********************"
-      # puts @something
-      # puts "**********************"
-      # puts ""
-    end
-  end
-end
-
-class ActiveRecord::base
-  include Sunbutter
 end
